812b28d368f9865cc04ddca3989da2729707c870
SemaseMing <admin@v-api.cn>
2016-08-18 18:27:39 +0800

    更新作业

--- day1/homework/homework2 copy.py
+++ day1/homework/homework2 copy.py
@@ -4,11 +4,11 @@
 # Time:2016-08-17 22:23
 
 # 三级菜单。
-# b：返回上一级 q:退出整个程序
+# b：返回上一级 q:退出整个程序 y:到第一级
+
 import copy
 
 # 还是一个总的菜单呢
-# menu_list = {1:{'name':'中国','province':{1:{'name':'北京','city':{1:{'name':'北京市','info':'北京是中国的首都'}}},2:{'name':'上海','city':{1:{'name':'上海市','info':'上海是一个非常有魅力的地方，我想去的地方'}}}}},2:{'name':'美国','province':{1:{'name':'加州','city':{1:{'name':'洛杉矶','info':'天使之城'},2:{'name':'旧金山','info':'旧金山唐人街'}}}}}}
 menu_list = {
     1: {
         'name': '中国'
@@ -54,19 +54,23 @@ menu_list = {
 }
 menu_tmp = {} #初始化临时meun
 menu = copy.deepcopy(menu_list) #拷贝菜单
-menu_last_1 = {} #初始化上级菜单
-menu_last_2 = {} #初始化上级菜单
-falg = True #Ture为到下-级 false到上一级
+menu_last = {} #初始化上级菜单
 cengji = 0 #初始化层级
-choose = 0 #初始化选择
+choose = 0 #初始化输入
 z = '' #初始化字符串
+shurulist = ['1','2','3','4','5','6','7','8','9','q','b','y'] #允许输入的字符列表
 
-print('欢迎来到级菜单')
+
+print("欢迎来到级菜单")
+print("提示：数字:进入下一级别 b:返回上一级 y:返回第一级 q:退出")
 while True:
-    print('现在的层级为：',cengji)
+    #print('现在的层级为：',cengji)
     for i in menu:
         print(i, '.', menu[i]['name'])
     choose = input('请输入您的选择:')
+    if choose not in shurulist:
+        print('您的输入不在范围之内,请重新输入')
+        continue
     if choose == 'q':
         print('退出')
         break
@@ -83,11 +87,11 @@ while True:
         menu = copy.deepcopy(menu_list)
         cengji = 0
         continue
-    elif (int(choose) not in menu):
+    elif (int(choose) in menu):
+        cengji += 1
+    else:
         print('您的输入不在范围之内')
         continue
-    else:
-        cengji += 1
 
     if cengji <= 0:
         print('这已经是第一层了,不能再上一层了,请重新选择')
