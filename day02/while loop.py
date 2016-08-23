7c9bd312b378232c6588986ae6b72e994f851d7a
SemaseMing <admin@v-api.cn>
2016-08-23 22:59:06 +0800

    提交笔记

---
+++ day02/while loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# Author:SemaseMing <blog.v-api.cn>
+# Time:2016-08-23 21:31
+
+count = 0
+while True:
+    print("你是风儿，我是沙，缠缠绵绵到天涯",count)
+    count += 1
