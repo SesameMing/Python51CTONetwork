6301888c4d5fffbd46bcb0788ade97087255e4de
SemaseMing <admin@v-api.cn>
2016-08-26 20:17:47 +0800

    记录笔记

--- day03/set.py
+++ day03/set.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 # Author:SemaseMing <blog.v-api.cn>
 # Time:2016-08-26 18:01
+
+# set,集合,无序，不重复序列
+
+'''
+# 创建
+li = []  # 列表
+dic = {}  # 字典
+se = {"123", "456"}  # 集合 创建集合方式1
+s = set()  # 创建一个空集合 创建集合方式2
+li = [11, 22, 11, 22]
+s1 = set(li)  # 将列表转化成集合 创建集合方式3
+print(s1)
+'''
+# 功能
+
+
+## 操作集合
+s = set()
+
+s.add(123)  # 添加元素
+s.clear()  # 清除元素
