d5eb832fa987110c61aa7569018ce66a0d919959
SemaseMing <admin@v-api.cn>
2016-08-27 23:41:17 +0800

    更新笔记

---
+++ day03/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+# Author:SemaseMing <blog.v-api.cn>
+# Time:2016-08-27 23:33
+
+# 测试 方法
+s1 = {11,22,33}
+s2 = {22,33,44}
+
+s3 = s1.union(s2)
+print(s1)
+print(s2)
+print(s3)
