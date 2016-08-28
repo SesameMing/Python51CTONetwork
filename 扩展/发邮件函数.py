dca5873848a2cf179b19150de2ea5e736b1cbe63
SemaseMing <admin@v-api.cn>
2016-08-28 11:37:45 +0800

    更新笔记

--- 扩展/发邮件函数.py
+++ 扩展/发邮件函数.py
@@ -7,12 +7,12 @@ import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-msg = MIMEText('大明 这是一封测试邮件', 'plain', 'utf-8')
-msg['From'] = formataddr(["大明", '15171888087@163.com'])
-msg['To'] = formataddr(["大明api", 'admin@v-api.com'])
+msg = MIMEText('SemaseMing 这是一封测试邮件', 'plain', 'utf-8')
+msg['From'] = formataddr(["SemaseMing", '发件邮箱'])
+msg['To'] = formataddr(["SemaseMing", '收件邮箱'])
 msg['Subject'] = "python邮件函数测试"
 
 server = smtplib.SMTP("smtp.163.com", 25)
-server.login("15171888087@163.com", "7833922")
-server.sendmail('15171888087@163.com', ['admin@v-api.com', ], msg.as_string())
+server.login("发件邮箱", "密码")
+server.sendmail('发件邮箱', ['收件邮箱', ], msg.as_string())
 server.quit()
