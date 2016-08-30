5c34a051cc3bd66263b4119fe67b7e8b448408a0
SemaseMing <admin@v-api.cn>
2016-08-30 19:56:15 +0800

    更新作业

--- day03/homework/HAproxy配置文件操作.py
+++ day03/homework/HAproxy配置文件操作.py
@@ -19,50 +19,81 @@ else:
 
 def getinfo():
     """ 获取HAproxy配置信息 """
-    pass
+    print(" 获取backend 和sever信息 ")
+    f = open(CFG_FILE, 'r+', encoding='utf-8')
+    for i in f:
+        print(i.strip())
 
 
 def addinfo():
     """ 添加HAproxy配置信息 """
-    pass
+    print(" 添加backend 和sever信息 ")
 
 
 def updateinfo():
     """ 修改HAproxy配置信息 """
-    pass
+    print(" 修改backend 和sever信息 ")
 
 
 def delinfo():
     """ 删除HAproxy配置信息 """
-    pass
+    print(" 删除backend 和sever信息 ")
 
 
 def bakupinfo():
     """ 备份HAproxy配置文件 """
-    pass
+    print(" 备份配置文件 ")
 
 
-def main():
-    str = """
-    ------------------------------------
-    |  欢迎来到HAproxy管理脚本  v1.0.0 |
-    ------------------------------------
-    |    1.获取backend 和sever信息     |
-    |    2.添加backend 和sever信息     |
-    |    3.修改backend 和sever信息     |
-    |    4.删除backend 和sever信息     |
-    |    5.备份配置文件                |
-    |    6.回滚配置文件                |
-    ------------------------------------
-    |   %s：退出         数字：选择     |
-    ------------------------------------
-    """ % QUIT_CHAR
-    while flag:
-        print(str)
-        num = input("请输入您的选择：")
-        if num.isdigit():
-            pass
+def rollback():
+    """ 回滚HAproxy配置文件 """
+    print(" 回滚HAproxy配置文件 ")
+
+
+str1 = """
+------------------------------------
+|  欢迎来到HAproxy管理脚本  v1.0.0 |
+------------------------------------
+|    1.获取backend 和sever信息     |
+|    2.添加backend 和sever信息     |
+|    3.修改backend 和sever信息     |
+|    4.删除backend 和sever信息     |
+|    5.备份配置文件                |
+|    6.回滚配置文件                |
+------------------------------------
+|   %s：退出         数字：选择     |
+------------------------------------
+""" % QUIT_CHAR
+while flag:
+    print(str1)
+    num = input("请输入您的选择：")
+    if num.isdigit():
+        num_int = int(num)
+        if num_int == 1:
+            """ 获取backend 和sever信息 """
+            getinfo()
+        elif num_int == 2:
+            """ 添加backend 和sever信息 """
+            getinfo()
+
+        elif num_int == 3:
+            """ 修改backend 和sever信息 """
+            updateinfo()
+        elif num_int == 4:
+            """ 删除backend 和sever信息 """
+            delinfo()
+
+        elif num_int == 5:
+            """ 备份配置文件 """
+            bakupinfo()
+
+        elif num_int == 6:
+            """ 回滚配置文件 """
+            rollback()
         else:
-            pass
+            print("不正确的选项，请重新选择")
+    else:
+        if num == QUIT_CHAR:
+            print("退出")
+            flag = False
 
-main()
