e6df3eef495e078a34fd963d0de21f088ab804c6
SemaseMing <admin@v-api.cn>
2016-09-24 00:53:26 +0800

    更新

---
+++ day06/正则表达式.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+# Author:SemaseMing <blog.v-api.cn>
+# Email: admin@v-api.cn
+# Time:2016-09-23 21:56
+import re
+# 元字符
+# . 通配符 除换行符以外的
+# ^ 以什么开头
+# $ 以什么结束
+# * + ？ ｛｝ 都是代表重复
+# * 匹配前一个0到多次重复
+# + 匹配前一个1到多次重复
+# ? 匹配前一个0或1次重复
+# {} 匹配前一个如意次数或者范围
+# []
+print(re.findall(r'I\b', 'I#am DamIng'))
