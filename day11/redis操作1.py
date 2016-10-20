61f197df351ebcc6ccdc7385bc713893b14074fc
SesameMing <admin@v-api.cn>
2016-10-20 18:47:31 +0800

    更新

--- day11/redis操作1.py
+++ day11/redis操作1.py
@@ -6,6 +6,6 @@
 
 import redis
 
-r = redis.Redis(host='192.168.199.213', port=6379)
+r = redis.Redis(host='127.0.0.1', port=6379)
 r.set('foo', 'Bar')
-print(r.get('foo'))
+print(r.get('foo').decode())
