7dca579db7d1a7b92f18163c97d04639d1ec001e
SesameMing <admin@v-api.cn>
2016-10-20 16:35:02 +0800

    更新

--- day11/memcache操作1.py
+++ day11/memcache操作1.py
@@ -7,9 +7,6 @@
 import memcache
 
 mc = memcache.Client(['192.168.199.213:12000'], debug=True)
-# mc.set("foo", "bar")
-# mc.set("foo1", "bar1")
-# mc.set("foo2", "bar2")
-# mc.set("foo3", "bar3")
-ret = mc.get("foo2")
+mc.set("foo", "bar")
+ret = mc.get("foo")
 print(ret)
