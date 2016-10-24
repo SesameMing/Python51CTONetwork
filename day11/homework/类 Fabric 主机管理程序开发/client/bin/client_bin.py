2fabaef75071762a28e08cd4343c038fb46edce1
SesameMing <admin@v-api.cn>
2016-10-24 21:28:56 +0800

    更新

--- day11/homework/类 Fabric 主机管理程序开发/client/bin/client_bin.py
+++ day11/homework/类 Fabric 主机管理程序开发/client/bin/client_bin.py
@@ -26,9 +26,6 @@ def setloggin():
 
 Log = setloggin()
 
-
-
-
 def login():
     Log.info("用户登录")
     time.sleep(.1)
@@ -56,6 +53,98 @@ def check_login(func):
         return func()
 
 
+# 下发任务
+def xiafaru():
+    pass
+
+
+# 链接服务器
+def lianjiefuwuqi():
+    if os.path.exists(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'], 'host.config')):
+        host_dic = json.load(open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'],
+                                               'host.config'), 'r'))
+        host_list = []
+        for item in host_dic:
+            for hostlist in host_dic[item]:
+                host_list.append(hostlist)
+        for index, hosts in enumerate(host_list):
+            print("%s. %s ip:%s " % (index, hosts[0], hosts[1]))
+        innp = input("选择要链接的服务器序号<q:退出>>>>：").strip()
+        if innp.isdigit():
+            if int(innp) < len(host_list):
+                host_action = host.HostClent(host_list[int(innp)][1], int(host_list[int(innp)][2]), host_list[int(innp)][3], host_list[int(innp)][4])
+                if host_action.ssh_host():
+                    Log.info("链接成功")
+                    while True:
+                        # cd 方法转换成ls 方法
+                        innp_commend = input("[%s@%s]:" % (host_list[int(innp)][3], host_list[int(innp)][0]))
+
+                        print(host_action.ssh_command(innp_commend))
+                else:
+                    Log.warning("链接失败，请检查网络是否通畅或者服务器帐号密码是否正确")
+            else:
+                Log.warning("输入的不在范围内")
+        else:
+            pass
+    else:
+        Log.warning("不存在主机,请先添加主机")
+
+
+# 显示分组
+def showclass():
+    flag = True
+    while flag:
+        if os.path.exists(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'], 'host.config')):
+            host_dic = json.load(open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'],
+                                                   'host.config'), 'r'))
+            print("分组")
+            classname = []
+            for item in host_dic:
+                classname.append(item)
+            for index, hostclass in enumerate(classname):
+                print("└ %s. %s ip:%s " % (index, hostclass[0], hostclass[1]))
+            print_str = """---------------------------
+数字：查看分类下的主机
+add: 添加分类 del：删除分类 edt：修改分类名称
+其他：退出
+"""
+            print(print_str)
+            innp = input(">>>:").strip()
+            if innp == 'add':
+                addhostclass()
+            elif innp == 'del':
+                n_innp = input("输入要删除的分组id序号>>>:").strip()
+                if int(n_innp) < len(classname):
+                    t_inpp = input("确定要删除分类【%s】以及分类下的主机吗？<y/n>" % classname[int(n_innp)]).strip()
+                    if t_inpp == 'y':
+                        del host_dic[classname[int(n_innp)]]
+                        json.dump(host_dic, open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'],
+                                                              'host.config'), 'w'))
+                        Log.info('删除分组成功')
+                        time.sleep(.1)
+                    print(classname[int(n_innp)])
+                else:
+                    print("输入的数字不在范围内")
+
+            elif innp == 'edt':
+                n_innp = input("输入要修改的分组id序号>>>:").strip()
+                if int(n_innp) < len(classname):
+                    t_inpp = input("修改分类【%s】的名称为：" % classname[int(n_innp)]).strip()
+                    if t_inpp not in classname:
+                        host_dic[t_inpp] = host_dic[classname[int(n_innp)]]
+                        del host_dic[classname[int(n_innp)]]
+                        json.dump(host_dic, open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'],
+                                                              'host.config'), 'w'))
+                        Log.info("新名称修改成功")
+                    else:
+                        Log.warning("新名称在分类中已经存在")
+                else:
+                    print("输入的数字不在范围内")
+
+            else:
+                flag = False
+                continue
+
 # 添加主机分组
 def addhostclass():
     while True:
@@ -81,26 +170,46 @@ def addhost():
         host_dic = json.load(
             open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'], 'host.config'), 'r'))
         if len(host_dic) > 0:
-            innp_name = input("准备输入主机名称(备注)>>>：")
-            innp_ip = input("准备输入主机ip>>>：")
-            innp_port = input("准备输入主机端口)>>>：")
+            innp_name = input("准备输入主机名称(备注)：").strip()
+            while bool(innp_name) is False:
+                innp_name = input("主机名称(备注)不能为空：").strip()
+            innp_ip = input("准备输入主机ip：").strip()
+            while bool(innp_ip) is False:
+                innp_ip = input("ip不能为空：").strip()
+            innp_port = input("准备输入主机端口：").strip()
+            while bool(innp_port) is False:
+                innp_port = 22
+                print("默认端口号为:22")
+            innp_username = input("请输入主机帐号：").strip()
+            while bool(innp_username) is False:
+                innp_username = input("主机帐号不能为空：").strip()
+            innp_password = input("请输入主机密码：").strip()
+            while bool(innp_password) is False:
+                innp_password = input("主机密码不能为空：").strip()
             Log.info("正在验证主机是否存在，请稍等..")
-            if host.yzhost(innp_ip, innp_port):
-                while True:
-                    Log.info("验证权限")
-                    innp_username = input("请输入主机帐号：")
-                    innp_password = input("请输入主机密码：")
-                    Log.info("正在验证主机权限，请等待...")
-                    if host.yzauth(innp_ip, innp_port, innp_username, innp_password):
-                        pass
-                    else:
-                        Log.warning("帐号密码验证错误")
-                        time.sleep(.1)
-                        continue
-
-    Log.warning("还没设置主机分组，请先添加主机分组")
-    time.sleep(.1)
-    addhostclass()
+            host_action = host.HostClent(innp_ip, innp_port, innp_username, innp_password)
+            if host_action.ssh_host():
+                Log.info("验证成功,请选择分类:")
+                classname = []
+                for item in host_dic:
+                    classname.append(item)
+                for index, items in enumerate(classname):
+                    print("%s . %s" % (index, items))
+                classnum = input(">>>:")
+                if int(classnum) < len(classname):
+                    host_dic[classname[int(classnum)]].append([innp_name, innp_ip, innp_port, innp_username, innp_password])
+                    json.dump(host_dic,
+                              open(os.path.join(setting.USER_DIR_PATH, USER_LOGIN_STATUS['username'],
+                                                'host.config'), 'w'))
+                    Log.info("添加成功")
+                    time.sleep(.1)
+            else:
+                Log.info("验证失败")
+                time.sleep(.1)
+    else:
+        Log.warning("还没设置主机分组，请先添加主机分组")
+        time.sleep(.1)
+        addhostclass()
 
 
 # 显示主机列表
@@ -112,11 +221,10 @@ def showhost():
         for hostclass in host_dic:
             print("分组 ：%s" % hostclass)
             if len(host_dic.get(hostclass)) == 0:
-                print(" └    暂无主机")
+                print(" └  暂无主机")
             else:
                 for hostlist in host_dic.get(hostclass):
-                    print(" └  主机 ：%s" % hostlist)
-
+                    print(" └ 主机: %s ip:%s " % (hostlist[0], hostlist[1]))
         print("------------------------")
         print("1.添加主机  其他：返回主菜单")
         innp = input(">>>:").strip()
@@ -162,9 +270,11 @@ def main():
         if inpp == '1':
             showhost()
         elif inpp == '2':
-            pass
+            showclass()
         elif inpp == '3':
             pass
+        elif inpp == '4':
+            lianjiefuwuqi()
         elif inpp == 'q':
             Log.info("退出程序")
             break
--- day11/homework/类 Fabric 主机管理程序开发/client/bin/lib/host.py
+++ day11/homework/类 Fabric 主机管理程序开发/client/bin/lib/host.py
@@ -4,34 +4,80 @@
 # Email: admin@v-api.cn
 # Time: 2016-10-23 20:00
 import socket
+import paramiko
 
 
-# 验证主机是否存在
-def yzhost(ip, port):
+# 验证主机是否能链接上
+def ssh_host(ip, port, user, passwd):
     try:
-        s = socket.socket()
-        s.connect((ip, port))
-        s.send(bytes('areyouready?'))
-        recv = s.recv(1024).decode()
-        s.close()
-        if recv == 'yes':
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(ip, port=int(port), username=user, password=passwd)
+        ssh_session = client.get_transport().open_session()
+        if ssh_session.active:
+            client.close()
             return True
         else:
             return False
     except:
         return False
 
-# 验证主机权限（帐号密码）
-def yzauth(ip, port, username, password):
+
+# 主机执行操作
+def ssh_command(ip, port, user, passwd, command):
     try:
-        s = socket.socket()
-        s.connect((ip, port))
-        s.send(bytes('areyouready?'))
-        recv = s.recv(1024).decode()
-        s.close()
-        if recv == 'yes':
-            return True
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(ip, port=int(port), username=user, password=passwd)
+        ssh_session = client.get_transport().open_session()
+        if ssh_session.active:
+            ssh_session.exec_command(command)
+            return ssh_session.recv(1024).decode()
         else:
             return False
-    except:
+    except Exception as e:
+        print(e)
         return False
+
+
+class HostClent():
+    def __init__(self, ip, port, user, passwd):
+        self.ip = ip
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+
+    # 链接主机并保留链接状态
+    def ssh_host(self):
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(self.ip, port=int(self.port), username=self.user, password=self.passwd)
+            ssh_session = client.get_transport().open_session()
+            self.client = client
+            self.ssh_session = ssh_session
+            if ssh_session.active:
+                return True
+            else:
+                return False
+        except Exception as e:
+            return False
+
+    # 主机执行操作
+    def ssh_command(self, command):
+        ssh_session = self.client.get_transport().open_session()
+        try:
+            if ssh_session.active:
+                ssh_session.exec_command(command)
+                recv_data = b""
+                while True:
+                    data = ssh_session.recv(1024)
+                    if not data:
+                        break
+                    else:
+                        recv_data += data
+                return recv_data.decode()
+            else:
+                return False
+        except Exception as e:
+            return False
